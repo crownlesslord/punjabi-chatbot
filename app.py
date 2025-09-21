1cbc4284b9bb3dfb920f0754ce9d442a5d148dfb
crownlesslord <128445584+crownlesslord@users.noreply.github.com>
2025-09-21 07:52:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ import os
 # -------------------------
 # Groq API Key
 # -------------------------
-groq_api_key = "GROQ_API_KEY"
+from dotenv import load_dotenv
+load_dotenv()
+
+groq_api_key = os.getenv("GROQ_API_KEY")
 
 # Initialize Groq client
 try:
